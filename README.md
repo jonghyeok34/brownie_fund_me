6e6eb1f209ad7f053ec234e11042d951f8d32d3f
jonghyeok34 <jonghyeok34@gmail.com>
2022-02-04 03:05:47 +0900

    use local ganache

--- README.md
+++ README.md
@@ -17,5 +17,23 @@ brownie run scripts/deploy
 brownie run scripts/deploy --network rinkeby
 ```
 [chain link mix](https://github.com/smartcontractkit/chainlink-mix)
+
+
+- ganache > setting > server
 ```
+hostname : 127.0.0.1 - loopback psuedo interface
+port number : 8545
+
 ```
+
+```
+brownie networks list
+# add ganache-local
+brownie networks add Ethereum ganache-local host=http://127.0.0.1:7545 chainid=5777
+# ganache-local 실행
+brownie run scripts/deploy.py --network ganache-local
+
+```
+
+
+
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import FundMe, MockV3Aggregator,  network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, deploy_mocks, get_account
 
 
 def deploy_fund_me():
@@ -8,23 +8,20 @@ def deploy_fund_me():
     
     # if we are on a persistent network like rinkeby, use the associated address
     # otherwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The acctive network is {network.show_active()}")
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000,{"from":account})
-        price_feed_address = mock_aggregator.address
-        print("Mocks deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
-
+    
 
 def main():
     deploy_fund_me()
